5f7d369e060b80133a7532731f09154b7f511270
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-09-13 13:52:40 +0200

    Move routes callbacks into RoutesCallbacks

---
+++ project/src/classes/routes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// TODO: remove this
+class RoutesCallbacks
+{
+    constructor()
+    {
+        server.addStartCallback("routeMissing", load);
+    }
+
+    load()
+    {
+        db.user.configs.accounts = "user/configs/accounts.json";
+        db.user.configs.gameplay = "user/configs/gameplay.json";
+        db.user.profiles = {
+            "character": "user/profiles/__REPLACEME__/character.json",
+            "dialogue": "user/profiles/__REPLACEME__/dialogue.json",
+            "storage": "user/profiles/__REPLACEME__/storage.json",
+            "userbuilds": "user/profiles/__REPLACEME__/userbuilds.json"
+        }
+    }
+}
+
+module.exports.routesCallbacks = new RoutesCallbacks();
