f3d2c7709ea5bc0934d699c5a297b0ba2207aac4
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-09-13 18:02:30 +0200

    Move keepalive callbacks to KeepAliveCallbacks

--- project/src/classes/keepalive.js
+++ project/src/classes/keepalive.js
@@ -1,12 +1,14 @@
 "use strict";
 
-function main(sessionID)
+function execute(sessionID)
 {
     if (!account_f.accountServer.isWiped(sessionID))
     {
         updateTraders(sessionID);
         updatePlayerHideout(sessionID);
     }
+
+    return {"msg": "OK"};
 }
 
 function updateTraders(sessionID)
@@ -287,4 +289,17 @@ function updateBitcoinFarm(btcProd, farmrecipe, btcFarmCGs, isGeneratorOn)
     return btcProd;
 }
 
-module.exports.main = main;
+class KeepAliveCallbacks
+{
+    constructor()
+    {
+        router.addStaticRoute("/client/game/keepalive", this.execute.bind());
+    }
+
+    execute(url, info, sessionID)
+    {
+        return response_f.getBody(keepAlive_f.execute(sessionID));
+    }
+}
+
+module.exports.KeepAliveCallbacks = new KeepAliveCallbacks();
