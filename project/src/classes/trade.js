3b3a92898a32db565568aef51aead9052cd8bd3a
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-09-13 17:32:24 +0200

    Refactor trade.js

--- project/src/classes/trade.js
+++ project/src/classes/trade.js
@@ -1,134 +1,153 @@
 "use strict";
 
-function buyItem(pmcData, body, sessionID)
+class TradeController
 {
-    if (body.tid === "579dc571d53a0658a154fbec")
+    buyItem(pmcData, body, sessionID)
     {
-        body.tid = "ragfair";
-    }
-
-    let callback = () =>
-    {
-        if (!itm_hf.payMoney(pmcData, body, sessionID))
+        if (body.tid === "579dc571d53a0658a154fbec")
         {
-            logger.logError("no money found");
-            throw "Transaction failed";
+            body.tid = "ragfair";
         }
-
-        logger.logSuccess("Bought item: " + body.item_id);
-    };
-
-    const newReq = {
-        "items": [{
-            "item_id": body.item_id,
-            "count": body.count,
-        }],
-        "tid": body.tid
-    };
-
-    return move_f.addItem(pmcData, newReq, item_f.itemServer.getOutput(), sessionID, callback);
-}
-
-// Selling item to trader
-function sellItem(pmcData, body, sessionID)
-{
-    let money = 0;
-    let prices = trader_f.traderServer.getPurchasesData(body.tid, sessionID);
-    let output = item_f.itemServer.getOutput();
-
-    for (let sellItem of body.items)
-    {
-        for (let item of pmcData.Inventory.items)
+    
+        let callback = () =>
         {
-            // profile inventory, look into it if item exist
-            let isThereSpace = sellItem.id.search(" ");
-            let checkID = sellItem.id;
-
-            if (isThereSpace !== -1)
+            if (!itm_hf.payMoney(pmcData, body, sessionID))
             {
-                checkID = checkID.substr(0, isThereSpace);
+                logger.logError("no money found");
+                throw "Transaction failed";
             }
-
-            // item found
-            if (item._id === checkID)
+    
+            logger.logSuccess("Bought item: " + body.item_id);
+        };
+    
+        const newReq = {
+            "items": [{
+                "item_id": body.item_id,
+                "count": body.count,
+            }],
+            "tid": body.tid
+        };
+    
+        return move_f.addItem(pmcData, newReq, item_f.itemServer.getOutput(), sessionID, callback);
+    }
+    
+    // Selling item to trader
+    sellItem(pmcData, body, sessionID)
+    {
+        let money = 0;
+        let prices = trader_f.traderServer.getPurchasesData(body.tid, sessionID);
+        let output = item_f.itemServer.getOutput();
+    
+        for (let sellItem of body.items)
+        {
+            for (let item of pmcData.Inventory.items)
             {
-                logger.logInfo("Selling: " + checkID);
-
-                // remove item
-                insurance_f.insuranceServer.remove(pmcData, checkID, sessionID);
-                output = move_f.removeItem(pmcData, checkID, output, sessionID);
-
-                // add money to return to the player
-                if (output !== "")
+                // profile inventory, look into it if item exist
+                let isThereSpace = sellItem.id.search(" ");
+                let checkID = sellItem.id;
+    
+                if (isThereSpace !== -1)
                 {
-                    money += parseInt(prices[item._id][0][0].count);
+                    checkID = checkID.substr(0, isThereSpace);
+                }
+    
+                // item found
+                if (item._id === checkID)
+                {
+                    logger.logInfo("Selling: " + checkID);
+    
+                    // remove item
+                    insurance_f.insuranceServer.remove(pmcData, checkID, sessionID);
+                    output = move_f.removeItem(pmcData, checkID, output, sessionID);
+    
+                    // add money to return to the player
+                    if (output !== "")
+                    {
+                        money += parseInt(prices[item._id][0][0].count);
+                        break;
+                    }
+    
+                    return "";
+                }
+            }
+        }
+    
+        // get money the item]
+        return itm_hf.getMoney(pmcData, money, body, output, sessionID);
+    }
+    
+    // separate is that selling or buying
+    confirmTrading(pmcData, body, sessionID)
+    {
+        // buying
+        if (body.type === "buy_from_trader")
+        {
+            return this.buyItem(pmcData, body, sessionID);
+        }
+    
+        // selling
+        if (body.type === "sell_to_trader")
+        {
+            return this.sellItem(pmcData, body, sessionID);
+        }
+    
+        return "";
+    }
+    
+    // Ragfair trading
+    confirmRagfairTrading(pmcData, body, sessionID)
+    {
+        let ragfair_offers_traders = database_f.database.tables.ragfair.offers;
+        let offers = body.offers;
+        let output = item_f.itemServer.getOutput();
+    
+        for (let offer of offers)
+        {
+            pmcData = profile_f.profileServer.getPmcProfile(sessionID);
+            body = {
+                "Action": "TradingConfirm",
+                "type": "buy_from_trader",
+                "tid": "ragfair",
+                "item_id": offer.id,
+                "count": offer.count,
+                "scheme_id": 0,
+                "scheme_items": offer.items
+            };
+    
+            for (let offerFromTrader of ragfair_offers_traders.offers)
+            {
+                if (offerFromTrader._id == offer.id)
+                {
+                    body.tid = offerFromTrader.user.id;
                     break;
                 }
-
-                return "";
             }
+    
+            output = this.confirmTrading(pmcData, body, sessionID);
         }
+    
+        return output;
     }
-
-    // get money the item]
-    return itm_hf.getMoney(pmcData, money, body, output, sessionID);
 }
 
-// separate is that selling or buying
-function confirmTrading(pmcData, body, sessionID)
+class TradeCallbacks
 {
-    // buying
-    if (body.type === "buy_from_trader")
+    constructor()
     {
-        return buyItem(pmcData, body, sessionID);
+        item_f.itemServer.addRoute("TradingConfirm", this.processTrade.bind());
+        item_f.itemServer.addRoute("RagFairBuyOffer", this.processRagfairTrade.bind());
     }
 
-    // selling
-    if (body.type === "sell_to_trader")
+    processTrade(pmcData, body, sessionID)
     {
-        return sellItem(pmcData, body, sessionID);
+        return trade_f.tradeServer.confirmTrading(pmcData, body, sessionID)
     }
 
-    return "";
+    processRagfairTrade(pmcData, body, sessionID)
+    {
+        return trade_f.confirmRagfairTrading(pmcData, body, sessionID);
+    }
 }
 
-// Ragfair trading
-function confirmRagfairTrading(pmcData, body, sessionID)
-{
-    let ragfair_offers_traders = database_f.database.tables.ragfair.offers;
-    let offers = body.offers;
-    let output = item_f.itemServer.getOutput();
-
-    for (let offer of offers)
-    {
-        pmcData = profile_f.profileServer.getPmcProfile(sessionID);
-
-        body = {
-            "Action": "TradingConfirm",
-            "type": "buy_from_trader",
-            "tid": "ragfair",
-            "item_id": offer.id,
-            "count": offer.count,
-            "scheme_id": 0,
-            "scheme_items": offer.items
-        };
-
-        for (let offerFromTrader of ragfair_offers_traders.offers)
-        {
-            if (offerFromTrader._id == offer.id)
-            {
-                body.tid = offerFromTrader.user.id;
-                break;
-            }
-        }
-
-        output = confirmTrading(pmcData, body, sessionID);
-    }
-
-    return output;
-}
-
-module.exports.buyItem = buyItem;
-module.exports.sellItem = sellItem;
-module.exports.confirmTrading = confirmTrading;
-module.exports.confirmRagfairTrading = confirmRagfairTrading;
+module.exports.tradeController = new TradeController();
+module.exports.TradeCallbacks = new TradeCallbacks();
