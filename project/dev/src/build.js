3d6799721efb063e3df28353cd014c537ecef1ad
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-09-13 10:40:38 +0200

    Delete old server.exe during build

--- project/dev/src/build.js
+++ project/dev/src/build.js
@@ -4,14 +4,22 @@ const UPX = require('upx')('best')
 const { compile } = require('nexe');
 
 // compile the application
-console.log(">>>STARTING BUILD...");
+console.log("Building server");
+
+if (fs.existsSync("Server.exe"))
+{
+	console.log("Old server build detected, removing the file");
+	fs.unlinkSync('Server.exe');
+}
+
 compile({
 	input: 'core/main.js',
 	output: 'Server-Uncompressed',
 	build: false,
 	ico: 'dev/res/icon.ico'
 }).then(function(err) {
-	console.log(">>>Changing ICON...");
+	console.log("Changing icon");
+
 	childProcess.execFile('dev/bin/ResourceHacker.exe', [
 		'-open',
 		'Server-Uncompressed.exe',
@@ -25,7 +33,9 @@ compile({
 		'ICONGROUP,MAINICON,'
 	], function(err) {
 		fs.unlinkSync('Server-Uncompressed.exe');
-		console.log(">>>Compressing Executable...");
+
+		console.log("Compress executable");
+
 		UPX('Server-Icon.exe')
 		.output('Server.exe')
 		.start().then(function(stats) {
