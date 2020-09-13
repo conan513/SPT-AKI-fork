8afd4f6661eb6fbe79a8281c3d63ef8c2f6183df
Terkoiz <martgest@gmail.com>
2020-09-13 23:07:01 +0300

    One more after-merge fix

--- project/src/keepalive.js
+++ project/src/keepalive.js
@@ -49,7 +49,7 @@ class KeepAliveCallbacks
 
     execute(url, info, sessionID)
     {
-        return response_f.getBody(keepalive_f.execute(sessionID));
+        return response_f.getBody(execute(sessionID));
     }
 }
 
