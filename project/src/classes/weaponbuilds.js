f5cd3acbbd468500a6b6acbe7128a401652f78b5
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-09-13 17:04:57 +0200

    Move weaponbuilds callbacks into WeaponBuildsCallbacks

---
+++ project/src/classes/weaponbuilds.js
@@ -0,0 +1,87 @@
+"use strict";
+
+class WeaponBuildsController
+{
+    getUserBuilds(sessionID)
+    {
+        let userBuildsMap = json.parse(json.read(this.getPath(sessionID)));
+        let userBuilds = [];
+    
+        for (let buildName in userBuildsMap)
+        {
+            userBuilds.push(userBuildsMap[buildName]);
+        }
+    
+        return userBuilds;
+    }
+    
+    SaveBuild(pmcData, body, sessionID)
+    {
+        delete body.Action;
+        body.id = utility.generateNewItemId();
+    
+        let output = item_f.itemServer.getOutput();
+        let savedBuilds = json.parse(json.read(this.getPath(sessionID)));
+    
+        // replace duplicate ID's. The first item is the base item.
+        // The root ID and the base item ID need to match.
+        body.items = itm_hf.replaceIDs(pmcData, body.items, false);
+        body.root = body.items[0]._id;
+    
+        savedBuilds[body.name] = body;
+        json.write(this.getPath(sessionID), savedBuilds);
+        output.builds.push(body);
+        return output;
+    }
+    
+    RemoveBuild(pmcData, body, sessionID)
+    {
+        let savedBuilds = json.parse(json.read(this.getPath(sessionID)));
+    
+        for (let name in savedBuilds)
+        {
+            if (savedBuilds[name].id === body.id)
+            {
+                delete savedBuilds[name];
+                json.write(this.getPath(sessionID), savedBuilds);
+                break;
+            }
+        }
+    
+        return item_f.itemServer.getOutput();
+    }
+    
+    getPath(sessionID)
+    {
+        let path = db.user.profiles.userbuilds;
+        return path.replace("__REPLACEME__", sessionID);
+    }
+}
+
+class WeaponBuildsCallbacks
+{
+    constructor()
+    {
+        router.addStaticRoute("/client/handbook/builds/my/list", this.getHandbookUserlist.bind());
+        item_f.itemServer.addRoute("SaveBuild", this.saveBuild.bind());
+        item_f.itemServer.addRoute("RemoveBuild", this.removeBuild.bind());
+    }
+
+    getHandbookUserlist(url, info, sessionID)
+    {
+        return response_f.getBody(weaponBuilds_f.weaponBuildsController.getUserBuilds(sessionID));
+    }
+
+    saveBuild(pmcData, body, sessionID)
+    {
+        return weaponBuilds_f.weaponBuildsController.saveBuild(pmcData, body, sessionID);
+    }
+
+    removeBuild(pmcData, body, sessionID)
+    {
+        return weaponBuilds_f.weaponBuildsController.removeBuild(pmcData, body, sessionID);
+    }
+}
+
+module.exports.weaponBuildsController = new WeaponBuildsController();
+module.exports.weaponBuildsCallbacks = new WeaponBuildsCallbacks();
