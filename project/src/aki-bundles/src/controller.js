e0b67e4ccb69cdab4f79d6ba3aeb5f1f35ee7ac4
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-09 11:15:59 +0200

    Refactor bundles

--- project/src/aki-bundles/src/controller.js
+++ project/src/aki-bundles/src/controller.js
@@ -1,4 +1,4 @@
-/* bundles.js
+/* controller.js
  * license: NCSA
  * copyright: Senko's Pub
  * website: https://www.guilded.gg/senkospub
@@ -102,44 +102,4 @@ class Controller
     }
 }
 
-class Callbacks
-{
-    constructor()
-    {
-        server_f.server.startCallback["loadBundles"] = this.load.bind(this);
-        server_f.server.respondCallback["BUNDLE"] = this.sendBundle.bind(this);
-        router_f.router.staticRoutes["/singleplayer/bundles/"] = this.getBundles.bind(this);
-        router_f.router.dynamicRoutes[".bundle"] = this.getBundle.bind(this);
-    }
-
-    load()
-    {
-        bundles_f.controller.initialize();
-    }
-
-    sendBundle(sessionID, req, resp, body)
-    {
-        logger_f.instance.logInfo("[BUNDLE]:" + req.url);
-
-        let bundleKey = req.url.split("/bundle/")[1];
-        let bundle = bundles_f.controller.getBundleByKey(bundleKey, true);
-        let path = bundle.path;
-
-        // send bundle
-        server_f.server.sendFile(resp, path);
-    }
-
-    getBundles(url, info, sessionID)
-    {
-        const local = (server_f.server.ip === "127.0.0.1" || server_f.server.ip === "localhost");
-        return response_f.controller.noBody(bundles_f.controller.getBundles(local));
-    }
-
-    getBundle(url, info, sessionID)
-    {
-        return "BUNDLE";
-    }
-}
-
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
+module.exports.Controller = Controller;
