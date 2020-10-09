4b45bd99d4cf1f90c8233e36d87a5a0b43aa553f
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-09 13:52:44 +0200

    Movee eft-images into eft-database

--- project/packages/eft-database/src/callbacks.js
+++ project/packages/eft-database/src/callbacks.js
@@ -14,6 +14,7 @@ class Callbacks
     constructor()
     {
         server_f.server.startCallback["loadDatabase"] = this.load.bind(this);
+        server_f.server.respondCallback["IMAGE"] = this.sendImage.bind(this);
         router_f.router.staticRoutes["/client/globals"] = this.getGlobals.bind(this);
         router_f.router.staticRoutes["/client/items"] = this.getTemplateItems.bind(this);
         router_f.router.staticRoutes["/client/handbook/templates"] = this.getTemplateHandbook.bind(this);
@@ -25,6 +26,8 @@ class Callbacks
         router_f.router.staticRoutes["/client/languages"] = this.getLocalesLanguages.bind(this);
         router_f.router.dynamicRoutes["/client/menu/locale/"] = this.getLocalesMenu.bind(this);
         router_f.router.dynamicRoutes["/client/locale/"] = this.getLocalesGlobal.bind(this);
+        router_f.router.dynamicRoutes[".jpg"] = this.getImage.bind(this);
+        router_f.router.dynamicRoutes[".png"] = this.getImage.bind(this);
     }
 
     load()
@@ -182,6 +185,11 @@ class Callbacks
         database_f.server.tables.bots = bots;
     }
 
+    getImage(url, info, sessionID)
+    {
+        return "IMAGE";
+    }
+
     getGlobals(url, info, sessionID)
     {
         database_f.server.tables.globals.time = Date.now() / 1000;
@@ -241,6 +249,43 @@ class Callbacks
     getLocalesGlobal(url, info, sessionID)
     {
         return response_f.controller.getUnclearedBody(database_f.server.tables.locales.global[url.replace("/client/locale/", "")]);
+    }
+
+    sendImage(sessionID, req, resp, body)
+    {
+        let splittedUrl = req.url.split("/");
+        let filename = splittedUrl[splittedUrl.length - 1].split(".").slice(0, -1).join(".");
+        let filepath = "packages/eft-database/res/";
+
+        // get images to look through
+        if (req.url.includes("/quest"))
+        {
+            logger_f.instance.logInfo("[IMG.quests]:" + req.url);
+            filepath += "quests";
+        }
+        else if (req.url.includes("/handbook"))
+        {
+            logger_f.instance.logInfo("[IMG.handbook]:" + req.url);
+            filepath += "handbook";
+        }
+        else if (req.url.includes("/avatar"))
+        {
+            logger_f.instance.logInfo("[IMG.trader]:" + req.url);
+            filepath += "traders";
+        }
+        else if (req.url.includes("banners"))
+        {
+            logger_f.instance.logInfo("[IMG.banners]:" + req.url);
+            filepath += "banners";
+        }
+        else
+        {
+            logger_f.instance.logInfo("[IMG.hideout]:" + req.url);
+            filepath += "hideout";
+        }
+
+        // send image
+        server_f.server.sendFile(resp, `${filepath}/${filename}.png`);
     }
 }
 
