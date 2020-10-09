3911e3b98976653a831b064e5acd696431cd8c43
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-10 00:29:43 +0200

    Refactor trader

--- project/packages/eft-traders/src/controller.js
+++ project/packages/eft-traders/src/controller.js
@@ -1,4 +1,4 @@
-/* trader.js
+/* controller.js
  * license: NCSA
  * copyright: Senko's Pub
  * website: https://www.guilded.gg/senkospub
@@ -9,7 +9,6 @@
 
 "use strict";
 
-/* controller class maintains list of traders for each sessionID in memory. */
 class Controller
 {
     constructor()
@@ -349,45 +348,4 @@ function traderFilter(traderFilters, tplToCheck)
     return false;
 }
 
-class Callbacks
-{
-    constructor()
-    {
-        router_f.router.staticRoutes["/client/trading/api/getTradersList"] = this.getTraderList.bind(this);
-        router_f.router.dynamicRoutes["/client/trading/api/getUserAssortPrice/trader/"] = this.getProfilePurchases.bind(this);
-        router_f.router.dynamicRoutes["/client/trading/api/getTrader/"] = this.getTrader.bind(this);
-        router_f.router.dynamicRoutes["/client/trading/api/getTraderAssort/"] = this.getAssort.bind(this);
-    }
-
-    getTraderList(url, info, sessionID)
-    {
-        return response_f.controller.getBody(trader_f.controller.getAllTraders(sessionID));
-    }
-
-    getProfilePurchases(url, info, sessionID)
-    {
-        return response_f.controller.getBody(trader_f.controller.getPurchasesData(url.substr(url.lastIndexOf("/") + 1), sessionID));
-    }
-
-    getTrader(url, info, sessionID)
-    {
-        return response_f.controller.getBody(trader_f.controller.getTrader(url.replace("/client/trading/api/getTrader/", ""), sessionID));
-    }
-
-    getAssort(url, info, sessionID)
-    {
-        return response_f.controller.getBody(trader_f.controller.getAssort(sessionID, url.replace("/client/trading/api/getTraderAssort/", "")));
-    }
-}
-
-class Config
-{
-    constructor()
-    {
-        this.fenceAssortSize = 60;
-    }
-}
-
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
-module.exports.config = new Config();
+module.exports.Controller = Controller;
