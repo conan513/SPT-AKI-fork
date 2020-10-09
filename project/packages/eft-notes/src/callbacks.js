19e80a274eef902b2d4fb47b650bd736540d81db
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-09 23:24:20 +0200

    Refactor notes

--- project/packages/eft-notes/src/callbacks.js
+++ project/packages/eft-notes/src/callbacks.js
@@ -1,42 +1,12 @@
-/* notes.js
+/* callbacks.js
  * license: NCSA
  * copyright: Senko's Pub
  * website: https://www.guilded.gg/senkospub
  * authors:
  * - Senko-san (Merijn Hendriks)
- * - BALIST0N
  */
 
 "use strict";
-
-class Controller
-{
-    addNote(pmcData, body, sessionID)
-    {
-        pmcData.Notes.Notes.push({
-            "Time": body.note.Time,
-            "Text": body.note.Text
-        });
-
-        return item_f.router.getOutput();
-    }
-
-    editNode(pmcData, body, sessionID)
-    {
-        pmcData.Notes.Notes[body.index] = {
-            "Time": body.note.Time,
-            "Text": body.note.Text
-        };
-
-        return item_f.router.getOutput();
-    }
-
-    deleteNote(pmcData, body, sessionID)
-    {
-        pmcData.Notes.Notes.splice(body.index, 1);
-        return item_f.router.getOutput();
-    }
-}
 
 class Callbacks
 {
@@ -63,5 +33,4 @@ class Callbacks
     }
 }
 
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
+module.exports.Callbacks = Callbacks;
