9756fdf97fe625fe7fe6a611f1fdda1f5207b544
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-09 13:25:26 +0200

    Refactor inraid

--- project/packages/eft-inraid/src/controller.js
+++ project/packages/eft-inraid/src/controller.js
@@ -1,4 +1,4 @@
-/* offraid.js
+/* controller.js
  * license: NCSA
  * copyright: Senko's Pub
  * website: https://www.guilded.gg/senkospub
@@ -374,70 +374,4 @@ class Controller
     }
 }
 
-class Callbacks
-{
-    constructor()
-    {
-        save_f.server.onLoadCallback["inraid"] = this.onLoad.bind(this);
-        router_f.router.staticRoutes["/raid/map/name"] = this.registerPlayer.bind(this);
-        router_f.router.staticRoutes["/raid/profile/save"] = this.saveProgress.bind(this);
-        router_f.router.staticRoutes["/singleplayer/settings/raid/endstate"] = this.getRaidEndState.bind(this);
-        router_f.router.staticRoutes["/singleplayer/settings/weapon/durability"] = this.getWeaponDurability.bind(this);
-        router_f.router.staticRoutes["/singleplayer/settings/raid/menu"] = this.getRaidMenuSettings.bind(this);
-    }
-
-    onLoad(sessionID)
-    {
-        return inraid_f.controller.onLoad(sessionID);
-    }
-
-    registerPlayer(url, info, sessionID)
-    {
-        inraid_f.controller.addPlayer(sessionID, info);
-        return response_f.controller.nullResponse();
-    }
-
-    saveProgress(url, info, sessionID)
-    {
-        inraid_f.controller.saveProgress(info, sessionID);
-        return response_f.controller.nullResponse();
-    }
-
-    getRaidEndState()
-    {
-        return response_f.controller.noBody(inraid_f.config.MIAOnRaidEnd);
-    }
-
-    getRaidMenuSettings(url, info, sessionID)
-    {
-        return response_f.controller.noBody(inraid_f.config.raidMenuSettings);
-    }
-
-    getWeaponDurability(url, info, sessionID)
-    {
-        return response_f.controller.noBody(inraid_f.config.save.durability);
-    }
-}
-
-class Config
-{
-    constructor()
-    {
-        this.MIAOnRaidEnd = false;
-        this.raidMenuSettings = {
-            "aiAmount": "AsOnline",
-            "aiDifficulty": "AsOnline",
-            "bossEnabled": true,
-            "scavWars": false,
-            "taggedAndCursed": false
-        };
-        this.save = {
-            "loot": true,
-            "durability": true
-        };
-    }
-}
-
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
-module.exports.config = new Config();
+module.exports.Controller = Controller;
