17b87b79cbc38ded391d83e0fad1591c421b794f
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-10 11:09:07 +0200

    Refactor quest

--- project/packages/eft-quest/src/controller.js
+++ project/packages/eft-quest/src/controller.js
@@ -1,4 +1,4 @@
-/* quest.js
+/* controller.js
  * license: NCSA
  * copyright: Senko's Pub
  * website: https://www.guilded.gg/senkospub
@@ -6,6 +6,8 @@
  * - Senko-san (Merijn Hendriks)
  * - BALIST0N
  * - Emperor06
+ * - Ginja
+ * - Ereshkigal
  */
 
 "use strict";
@@ -476,45 +478,4 @@ class Controller
     }
 }
 
-class Callbacks
-{
-    constructor()
-    {
-        router_f.router.staticRoutes["/client/quest/list"] = this.listQuests.bind(this);
-        item_f.router.routes["QuestAccept"] = this.acceptQuest.bind(this);
-        item_f.router.routes["QuestComplete"] = this.completeQuest.bind(this);
-        item_f.router.routes["QuestHandover"] = this.handoverQuest.bind(this);
-    }
-
-    acceptQuest(pmcData, body, sessionID)
-    {
-        return quest_f.controller.acceptQuest(pmcData, body, sessionID);
-    }
-
-    completeQuest(pmcData, body, sessionID)
-    {
-        return quest_f.controller.completeQuest(pmcData, body, sessionID);
-    }
-
-    handoverQuest(pmcData, body, sessionID)
-    {
-        return quest_f.controller.handoverQuest(pmcData, body, sessionID);
-    }
-
-    listQuests(url, info, sessionID)
-    {
-        return response_f.controller.getBody(quest_f.controller.getVisibleQuests(url, info, sessionID));
-    }
-}
-
-class Config
-{
-    constructor()
-    {
-        this.redeemTime = 48;
-    }
-}
-
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
-module.exports.config = new Config();
+module.exports.Controller = Controller;
