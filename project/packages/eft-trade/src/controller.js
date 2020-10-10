650473e778c37cea461e125b265137beca05be12
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-10 10:59:21 +0200

    Refactor trade

--- project/packages/eft-trade/src/controller.js
+++ project/packages/eft-trade/src/controller.js
@@ -1,4 +1,4 @@
-/* trade.js
+/* controller.js
  * license: NCSA
  * copyright: Senko's Pub
  * website: https://www.guilded.gg/senkospub
@@ -139,24 +139,4 @@ class Controller
     }
 }
 
-class Callbacks
-{
-    constructor()
-    {
-        item_f.router.routes["TradingConfirm"] = this.processTrade.bind(this);
-        item_f.router.routes["RagFairBuyOffer"] = this.processRagfairTrade.bind(this);
-    }
-
-    processTrade(pmcData, body, sessionID)
-    {
-        return trade_f.controller.confirmTrading(pmcData, body, sessionID);
-    }
-
-    processRagfairTrade(pmcData, body, sessionID)
-    {
-        return trade_f.controller.confirmRagfairTrading(pmcData, body, sessionID);
-    }
-}
-
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
+module.exports.Controller = Controller;
