5cafc5c7cd36f25db59dad6a66de0ca8191eaff5
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-10-12 12:38:46 +0200

    Fix hash generation

--- project/packages/aki-common/src/hash.js
+++ project/packages/aki-common/src/hash.js
@@ -17,7 +17,7 @@ class HashUtil
     generate()
     {
         const shasum = crypto.createHash("sha1");
-        const time = Math.random() * this.getTimestamp();
+        const time = Math.random() * common_f.time.getTimestamp();
 
         shasum.update(time.toString());
         return shasum.digest("hex").substring(0, 24);
