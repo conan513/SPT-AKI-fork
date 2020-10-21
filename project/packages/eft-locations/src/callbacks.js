904aad526488ecdd9ed631b3543e36110b95c8c2
Ginja <citax05@gmail.com>
2020-10-21 18:40:16 +0100

    Location data needs to be sent without data wrapping

--- project/packages/eft-locations/src/callbacks.js
+++ project/packages/eft-locations/src/callbacks.js
@@ -29,7 +29,7 @@ class Callbacks
 
     getLocation(url, info, sessionID)
     {
-        return https_f.response.getBody(location_f.controller.get(url.replace("/api/location/", "")));
+        return common_f.json.serialize(location_f.controller.get(url.replace("/api/location/", "")));
     }
 }
 
