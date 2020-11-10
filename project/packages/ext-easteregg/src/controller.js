234b5d7e2b153351cca6a063b85bee5283830662
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-11-10 20:56:03 +0100

    Revert "Merge pull request 'Because this shitshow is ridiculous and i'm bored of them.' (#174) from Ereshkigal/Server:development into development"
    This reverts commit 41927ac54f08d735fb0a929a9119b1e0d886e280, reversing
    changes made to 3ce6bfd951776b021e8a161d9145f146e1165c98.

--- project/packages/ext-easteregg/src/controller.js
+++ project/packages/ext-easteregg/src/controller.js
@@ -16,6 +16,7 @@ class Controller
         const names = [
             "senko",
             "ginja",
+            "ereshkigal",
             "wafflelord",
             "reider123",
             "terkoiz",
