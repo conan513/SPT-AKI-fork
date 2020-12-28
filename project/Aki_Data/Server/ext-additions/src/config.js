1bb71995420f2c9d2b8ff8fba7bf4a65b59f55a2
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2020-12-28 14:26:52 +0100

    Fix client errors caused by pmcgroups

--- project/Aki_Data/Server/ext-additions/src/config.js
+++ project/Aki_Data/Server/ext-additions/src/config.js
@@ -34,8 +34,8 @@ class Config
                 "follower": "hard"
             },
             "type": {
-                "boss": "bossTest",
-                "follower": "followerTest"
+                "boss": "assaultGroup",
+                "follower": "assaultGroup"
             },
             "locations": {
                 "bigmap": "ZoneBrige,ZoneCrossRoad,ZoneFactorySide,ZoneOldAZS,ZoneBlockPost,ZoneTankSquare,ZoneCustoms,ZoneDormitory,ZoneGasStation,ZoneFactoryCenter,ZoneWade,ZoneScavBase",
