c34e35c282933c2237b5f426a46935343c5589bd
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-02 13:36:34 +0100

    Rewrite image handler

--- project/Aki_Data/Server/aki-https/package.js
+++ project/Aki_Data/Server/aki-https/package.js
@@ -9,11 +9,13 @@
 const { Response } = require("./src/response.js");
 const { Router } = require("./src/router.js");
 const { Server } = require("./src/server.js");
+const { ImageHandler } = require("./src/image.js");
 const { Callbacks } = require("./src/callbacks.js");
 const { Config } = require("./src/config.js");
 
 module.exports.response = new Response();
 module.exports.router = new Router();
 module.exports.server = new Server();
+module.exports.image = new ImageHandler();
 module.exports.callbacks = new Callbacks();
 module.exports.config = new Config();
