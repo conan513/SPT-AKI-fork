cf61ecc892d8f71ce8ce68c63310a23930d404dc
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-04 00:19:38 +0100

    Restore ragfair settings

--- project/Aki_Data/Server/eft-ragfair/src/config.js
+++ project/Aki_Data/Server/eft-ragfair/src/config.js
@@ -19,11 +19,20 @@ class Config
             "enableFees": false
         };
         this.static = {
-            "unknown": true,
-            "traders": {}
+            "unknown": false,
+            "traders": {
+                "54cb50c76803fa8b248b4571": true,
+                "54cb57776803fa99248b456e": true,
+                "579dc571d53a0658a154fbec": true,
+                "58330581ace78e27b8b10cee": true,
+                "5935c25fb3acc3127c3d8cd9": true,
+                "5a7c2eca46aef81a7ca2145d": true,
+                "5ac3b934156ae10c4430e83c": true,
+                "5c0647fdd443bc2504c2d371": true
+            }
         };
         this.dynamic = {
-            "enabled": false,
+            "enabled": true,
             "threshold": 4000,
             "batchSize": 100,
             "priceMin": 0.95,
