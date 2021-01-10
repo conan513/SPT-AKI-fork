a7879d96adcc891cb133daf7cc78db246386cb9b
Simon Gavin <superbudvar@gmail.com>
2021-01-10 00:13:11 +0000

    added time interval of 15mins (same as keepalive was?)

--- project/Aki_Data/Server/eft-hideout/package.js
+++ project/Aki_Data/Server/eft-hideout/package.js
@@ -8,6 +8,8 @@
 
 const { Controller } = require("./src/controller.js");
 const { Callbacks } = require("./src/callbacks.js");
+const { Config } = require("./src/config.js");
 
 module.exports.controller = new Controller();
 module.exports.callbacks = new Callbacks();
+module.exports.config = new Config();
