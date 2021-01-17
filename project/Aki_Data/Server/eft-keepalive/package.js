294cfdd1b8109db4b3ffc8db32f83e88092cb823
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-17 20:14:45 +0100

    eft-keepalive class rename

--- project/Aki_Data/Server/eft-keepalive/package.js
+++ project/Aki_Data/Server/eft-keepalive/package.js
@@ -6,8 +6,6 @@
  * - Senko-san (Merijn Hendriks)
  */
 
-const { Controller } = require("./src/controller.js");
-const { Callbacks } = require("./src/callbacks.js");
+const { KeepaliveCallbacks } = require("./src/callbacks.js");
 
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
+module.exports.callbacks = new KeepaliveCallbacks();
