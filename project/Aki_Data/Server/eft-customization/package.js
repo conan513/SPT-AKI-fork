6f02694accad73e6cf0c44ec483fed17df3a3faa
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-17 19:55:34 +0100

    aki-customization class rename

--- project/Aki_Data/Server/eft-customization/package.js
+++ project/Aki_Data/Server/eft-customization/package.js
@@ -6,8 +6,8 @@
  * - Senko-san (Merijn Hendriks)
  */
 
-const { Controller } = require("./src/controller.js");
-const { Callbacks } = require("./src/callbacks.js");
+const { CustomizationController } = require("./src/controller.js");
+const { CustomizationCallbacks } = require("./src/callbacks.js");
 
-module.exports.controller = new Controller();
-module.exports.callbacks = new Callbacks();
+module.exports.controller = new CustomizationController();
+module.exports.callbacks = new CustomizationCallbacks();
