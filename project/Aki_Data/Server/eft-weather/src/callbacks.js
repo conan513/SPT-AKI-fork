f221447eef7a7c4a20f1efe5cde60f606fb61993
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-17 20:55:18 +0100

    eft-weather class rename

--- project/Aki_Data/Server/eft-weather/src/callbacks.js
+++ project/Aki_Data/Server/eft-weather/src/callbacks.js
@@ -8,7 +8,7 @@
 
 "use strict";
 
-class Callbacks
+class WeatherCallbacks
 {
     constructor()
     {
@@ -21,4 +21,4 @@ class Callbacks
     }
 }
 
-module.exports.Callbacks = Callbacks;
+module.exports.WeatherCallbacks = WeatherCallbacks;
