4bfd4772336ed7e27fa11cc2ab16614b4490c846
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-18 17:43:00 +0100

    eft-trader file rename

--- project/Aki_Data/Server/eft-traders/package.js
+++ project/Aki_Data/Server/eft-traders/package.js
@@ -6,9 +6,9 @@
  * - Senko-san (Merijn Hendriks)
  */
 
-const { TraderController } = require("./src/controller.js");
-const { TraderCallbacks } = require("./src/callbacks.js");
-const { TraderConfig } = require("./src/config.js");
+const { TraderController } = require("./src/TraderController.js");
+const { TraderCallbacks } = require("./src/TraderCallbacks.js");
+const { TraderConfig } = require("./src/TraderConfig.js");
 
 module.exports.controller = new TraderController();
 module.exports.callbacks = new TraderCallbacks();
