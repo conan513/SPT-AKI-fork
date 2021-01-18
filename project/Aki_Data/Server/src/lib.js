d58c8c045ed86574cd83b262044f3874d3ca521c
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-18 19:51:10 +0100

    WIP work dump

---
+++ project/Aki_Data/Server/src/lib.js
@@ -0,0 +1,174 @@
+// common_f
+
+global["common_f"] = {
+    // code here
+};
+
+// certs_f
+
+global["certs_f"] = {
+    // code here
+};
+
+// https_f
+
+global["https_f"] = {
+    // code here
+};
+
+// database_f
+
+global["database_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// item_f
+
+global["item_f"] = {
+    // code here
+};
+
+// single callbacks here
+// <eft-startup>
+// <eft-database>
+
+// mods_f
+const { ModController } = require("./controllers/ModController.js");
+const { ModCallbacks } = require("./callbacks/ModCallbacks.js");
+
+global["mods_f"] = {
+    "loader": new ModController(),
+    "callbacks": new ModCallbacks()
+};
