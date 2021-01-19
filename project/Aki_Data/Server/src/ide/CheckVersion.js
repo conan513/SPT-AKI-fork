6b8d8f99757da9746066ebc54772a7290e23ac57
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-19 11:34:16 +0100

    Rework build pt.2

--- project/Aki_Data/Server/src/ide/CheckVersion.js
+++ project/Aki_Data/Server/src/ide/CheckVersion.js
@@ -15,7 +15,5 @@ if (!semver.satisfies(process.version, "v12.18.2"))
     console.log(`Your version is ${process.version}`);
     process.exit(1);
 }
-else
-{
-    console.log("Your node version is correct.");
-}
+
+console.log("Your node version is correct.");
