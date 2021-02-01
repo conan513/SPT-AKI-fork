13bd51c75d33616cfa95700e7bc6f24ecdb4347d
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-02-01 20:44:37 +0100

    Remove callbacks from globals

--- project/src/Lib.js
+++ project/src/Lib.js
@@ -15,8 +15,7 @@ globalThis.ContainerHelper = require("./helpers/ContainerHelper");
 globalThis.Mods = require("./controllers/ModController");
 
 globalThis["certs_f"] = {
-    "controller": require("./controllers/CertController.js"),
-    "callbacks": require("./callbacks/CertCallbacks.js")
+    "controller": require("./controllers/CertController.js")
 };
 
 globalThis["https_f"] = {
@@ -24,8 +23,7 @@ globalThis["https_f"] = {
     "response": require("./utils/HttpResponse.js"),
     "router": require("./routers/HttpRouter.js"),
     "image": require("./routers/ImageRouter.js"),
-    "server": require("./servers/HttpServer.js"),
-    "callbacks": require("./callbacks/HttpCallbacks.js")
+    "server": require("./servers/HttpServer.js")
 };
 
 globalThis["database_f"] = {
@@ -34,152 +32,122 @@ globalThis["database_f"] = {
 };
 
 globalThis["item_f"] = {
-    "eventHandler": require("./routers/ItemEventRouter.js"),
-    "callbacks": require("./callbacks/ItemEventCallbacks.js")
+    "eventHandler": require("./routers/ItemEventRouter.js")
 };
 
 globalThis["save_f"] = {
-    "server": require("./servers/SaveServer.js"),
-    "callbacks": require("./callbacks/SaveCallbacks.js")
+    "server": require("./servers/SaveServer.js")
 };
 
 globalThis["health_f"] = {
     "config": require("./configs/HealthConfig.json"),
-    "controller": require("./controllers/HealthController.js"),
-    "callbacks": require("./callbacks/HealthCallbacks.js")
+    "controller": require("./controllers/HealthController.js")
 };
 
 globalThis["inraid_f"] = {
     "config": require("./configs/InraidConfig.json"),
-    "controller": require("./controllers/InraidController.js"),
-    "callbacks": require("./callbacks/InraidCallbacks.js")
+    "controller": require("./controllers/InraidController.js")
 };
 
 globalThis["dialogue_f"] = {
-    "controller": require("./controllers/DialogueController.js"),
-    "callbacks": require("./callbacks/DialogueCallbacks.js")
+    "controller": require("./controllers/DialogueController.js")
 };
 
 globalThis["account_f"] = {
-    "controller": require("./controllers/LauncherController.js"),
-    "callbacks": require("./callbacks/LauncherCallbacks.js")
+    "controller": require("./controllers/LauncherController.js")
 };
 
 globalThis["profile_f"] = {
-    "controller": require("./controllers/ProfileController.js"),
-    "callbacks": require("./callbacks/ProfileCallbacks.js")
+    "controller": require("./controllers/ProfileController.js")
 };
 
 globalThis["notifier_f"] = {
-    "controller": require("./controllers/NotifierController.js"),
-    "callbacks": require("./callbacks/NotifierCallbacks.js")
+    "controller": require("./controllers/NotifierController.js")
 };
 
 globalThis["bots_f"] = {
     "config": require("./configs/BotConfig.json"),
     "controller": require("./controllers/BotController.js"),
-    "generator": require("./generators/BotGenerator.js"),
-    "callbacks": require("./callbacks/BotCallbacks.js")
+    "generator": require("./generators/BotGenerator.js")
 };
 
 globalThis["quest_f"] = {
     "config": require("./configs/QuestConfig.json"),
     "helpers": require("./helpers/QuestHelpers.js"),
-    "controller": require("./controllers/QuestController.js"),
-    "callbacks": require("./callbacks/QuestCallbacks.js")
+    "controller": require("./controllers/QuestController.js")
 };
 
 globalThis["note_f"] = {
-    "controller": require("./controllers/NoteController.js"),
-    "callbacks": require("./callbacks/NoteCallbacks.js")
+    "controller": require("./controllers/NoteController.js")
 };
 
 globalThis["inventory_f"] = {
     "config": require("./configs/InventoryConfig.json"),
-    "controller": require("./controllers/InventoryController.js"),
-    "callbacks": require("./callbacks/InventoryCallbacks.js")
+    "controller": require("./controllers/InventoryController.js")
 };
 
 globalThis["wishList_f"] = {
-    "controller": require("./controllers/WishlistController.js"),
-    "callbacks": require("./callbacks/WishlistCallbacks.js")
+    "controller": require("./controllers/WishlistController.js")
 };
 
 globalThis["trade_f"] = {
-    "controller": require("./controllers/TradeController.js"),
-    "callbacks": require("./callbacks/TradeCallbacks.js")
+    "controller": require("./controllers/TradeController.js")
 };
 
 globalThis["customization_f"] = {
-    "controller": require("./controllers/CustomizationController.js"),
-    "callbacks": require("./callbacks/CustomizationCallbacks.js")
+    "controller": require("./controllers/CustomizationController.js")
 };
 
 globalThis["hideout_f"] = {
     "config": require("./configs/HideoutConfig.json"),
-    "controller": require("./controllers/HideoutController.js"),
-    "callbacks": require("./callbacks/HideoutCallbacks.js")
+    "controller": require("./controllers/HideoutController.js")
 };
 
 globalThis["weaponbuilds_f"] = {
-    "controller": require("./controllers/PresetBuildController.js"),
-    "callbacks": require("./callbacks/PresetBuildCallbacks.js")
+    "controller": require("./controllers/PresetBuildController.js")
 };
 
 globalThis["repair_f"] = {
     "config": require("./configs/RepairConfig.json"),
-    "controller": require("./controllers/RepairController.js"),
-    "callbacks": require("./callbacks/RepairCallbacks.js")
+    "controller": require("./controllers/RepairController.js")
 };
 
 globalThis["insurance_f"] = {
     "config": require("./configs/InsuranceConfig.json"),
-    "controller": require("./controllers/InsuranceController.js"),
-    "callbacks": require("./callbacks/InsuranceCallbacks.js")
+    "controller": require("./controllers/InsuranceController.js")
 };
 
 globalThis["trader_f"] = {
     "config": require("./configs/TraderConfig.json"),
-    "controller": require("./controllers/TraderController.js"),
-    "callbacks": require("./callbacks/TraderCallbacks.js")
+    "controller": require("./controllers/TraderController.js")
 };
 
 globalThis["preset_f"] = {
-    "controller": require("./controllers/PresetController.js"),
-    "callbacks": require("./callbacks/PresetCallbacks.js")
+    "controller": require("./controllers/PresetController.js")
 };
 
 globalThis["ragfair_f"] = {
     "config": require("./configs/RagfairConfig.json"),
     "server": require("./servers/RagfairServer.js"),
-    "controller": require("./controllers/RagfairController.js"),
-    "callbacks": require("./callbacks/RagfairCallbacks.js")
+    "controller": require("./controllers/RagfairController.js")
 };
 
 globalThis["weather_f"] = {
     "config": require("./configs/WeatherConfig.json"),
-    "controller": require("./controllers/WeatherController.js"),
-    "callbacks": require("./callbacks/WeatherCallbacks.js")
+    "controller": require("./controllers/WeatherController.js")
 };
 
 globalThis["location_f"] = {
     "config": require("./configs/LocationConfig.json"),
     "controller": require("./controllers/LocationController.js"),
-    "generator": require("./generators/LocationGenerator.js"),
-    "callbacks": require("./callbacks/LocationCallbacks.js")
-};
-
-globalThis["game_f"] = {
-    "callbacks": require("./callbacks/GameCallbacks.js")
+    "generator": require("./generators/LocationGenerator.js")
 };
 
 globalThis["match_f"] = {
     "config": require("./configs/MatchConfig.json"),
-    "controller": require("./controllers/MatchController.js"),
-    "callbacks": require("./callbacks/MatchCallbacks.js")
+    "controller": require("./controllers/MatchController.js")
 };
 
 globalThis["mods_f"] = {
-    "loader": require("./controllers/ModController.js"),
-    "callbacks": require("./callbacks/ModCallbacks.js")
+    "loader": require("./controllers/ModController.js")
 };
