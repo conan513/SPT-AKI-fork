d16e9200a95d11695cb29601b5834baeaa89363a
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-02-01 22:57:22 +0100

    Make watermark and app static

--- project/src/utils/App.js
+++ project/src/utils/App.js
@@ -10,58 +10,51 @@
 
 class App
 {
-    constructor()
-    {
-        this.onLoad = {};
-        this.onUpdate = {};
-        this.onUpdateLastRun = {};
-    }
+    static onLoad = {};
+    static onUpdate = {};
+    static onUpdateLastRun = {};
 
-    load()
+    static load()
     {
         // execute onLoad callbacks
         console.log("Server: executing startup callbacks...");
 
-        for (const callback in this.onLoad)
+        for (const callback in App.onLoad)
         {
-            this.onLoad[callback]();
+            App.onLoad[callback]();
         }
 
-        setInterval(this.update.bind(this), 1000);
+        setInterval(App.update, 1000);
     }
 
-    update()
+    static update()
     {
-        for (const taskId in this.onUpdate)
+        for (const taskId in App.onUpdate)
         {
-            let success,
-                lastruntime = 0;
-            if (this.onUpdateLastRun[taskId] !== undefined)
-            {
-                lastruntime = this.onUpdateLastRun[taskId];
-            }
+            let success = false;
+            let lastruntime = App.onUpdateLastRun[taskId] || 0;
 
             const timeSinceLastRun = TimeUtil.getTimestamp() - lastruntime;
 
             try
             {
-                success = this.onUpdate[taskId](timeSinceLastRun);
+                success = App.onUpdate[taskId](timeSinceLastRun);
             }
             catch (err)
             {
                 Logger.error(`Scheduled event: '${taskId}' failed to run successfully.`);
                 console.log(err);
-                success = false;
             }
 
             if (success)
             {
-                this.onUpdateLastRun[taskId] = TimeUtil.getTimestamp();
+                App.onUpdateLastRun[taskId] = TimeUtil.getTimestamp();
             }
             else
             {
                 /* temporary for debug */
                 const warnTime = 20 * 60;
+
                 if (success === void 0 && !(timeSinceLastRun % warnTime))
                 {
                     Logger.info(`onUpdate: ${taskId} doesn't report success or fail`);
@@ -71,4 +64,4 @@ class App
     }
 }
 
-module.exports = new App();
+module.exports = App;
