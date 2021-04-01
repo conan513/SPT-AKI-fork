95c66acd1b1c6f5b5841ad7cb88e0e89d57d2600
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-04-01 21:39:07 +0200

    Oops

--- project/src/configs/RagfairConfig.js
+++ project/src/configs/RagfairConfig.js
@@ -11,6 +11,7 @@ module.exports = {
         "traders": {
             "54cb50c76803fa8b248b4571": true,
             "54cb57776803fa99248b456e": true,
+            "579dc571d53a0658a154fbec": false,
             "58330581ace78e27b8b10cee": true,
             "5935c25fb3acc3127c3d8cd9": true,
             "5a7c2eca46aef81a7ca2145d": true,
