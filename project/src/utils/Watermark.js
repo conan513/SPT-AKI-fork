d567fed2ec7c485158317164d75b34f0e56167de
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-04-12 03:28:39 +0200

    Update watermark

--- project/src/utils/Watermark.js
+++ project/src/utils/Watermark.js
@@ -5,7 +5,7 @@ require("../Lib.js");
 class Watermark
 {
     static project = "SPT-AKI";
-    static version = "B3";
+    static version = "B4";
     static url = "https://www.guilded.gg/senkospub";
     static colors = {
         "front": "\x1b[33m",    // gold
