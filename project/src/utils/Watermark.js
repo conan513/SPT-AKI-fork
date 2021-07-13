7f82e0005c461bce4e3dad780aa06d94d1b489d0
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-07-13 15:04:46 +0200

    Bump version

--- project/src/utils/Watermark.js
+++ project/src/utils/Watermark.js
@@ -54,7 +54,7 @@ class WatermarkLocale
 class Watermark
 {
     static project = "SPT-AKI";
-    static version = "2.0.0";
+    static version = "2.0.0-A3";
     static text = [];
 
     static initialize()
