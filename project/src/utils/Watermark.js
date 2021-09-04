4e7f9ba222703f5ed81b95eb968baa2c364630f0
Terkoiz <martgest@gmail.com>
2021-09-04 17:37:38 +0300

    Version bump; Re-enabled mod loading

--- project/src/utils/Watermark.js
+++ project/src/utils/Watermark.js
@@ -54,7 +54,7 @@ class WatermarkLocale
 class Watermark
 {
     static project = "SPT-AKI";
-    static version = "2.0.0-A10";
+    static version = "2.0.0-RC1";
     static text = [];
     static versionLabel = "";
 
